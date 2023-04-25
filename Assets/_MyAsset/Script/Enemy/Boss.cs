cfcc0814ae4bdda5805acaf9f55cd9180b0312de
fchaf <Infinitrix@outlook.fr>
2023-04-25 15:52:54 -0400

    Push 1

---
+++ Assets/_MyAsset/Script/Enemy/Boss.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    [SerializeField] private float _speed = 10f;
+    [SerializeField] private GameObject _laserPreFab = default;
+    [SerializeField] private GameObject _missilePreFab = default;
+    [SerializeField] private float _fireRateM = 650f;
+    [SerializeField] private float _fireRate = 0.5f;
+    [SerializeField] int _playerLives = 5;
+
+    private float _canFire = -1f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/_MyAsset/Script/Manager/SpawnManager.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    //[SerializeField] private GameObject _enemyPrefab = default;
+    [SerializeField] private GameObject[] _listeEnemyPrefabs = default;
+    [SerializeField] private GameObject _ObjectContainer = default;
+    [SerializeField] private GameObject _miniBossContainer = default;
+    [SerializeField] private GameObject _BossContainer = default;
+    private bool _stopSpawning = false;
+
+    void Start()
+    {
+        StartSpawming();
+        //StartCoroutine(SpawnPURoutine());
+    }
+
+    /*
+    IEnumerator SpawnPURoutine()
+    {
+        yield return new WaitForSeconds(3.0f);
+        while (!_stopSpawning)
+        {
+            Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
+            int randomPU = Random.Range(0, _listePUsPrefabs.Length);
+            GameObject newPU = Instantiate(_listePUsPrefabs[randomPU], posToSpawn, Quaternion.identity);
+            newPU.transform.parent = _enemyContainer.transform;
+            int randomTime = Random.Range(10, 15);
+            yield return new WaitForSeconds(randomTime);
+
+        }
+    }
+    */
+
+    public void StartSpawming()
+    {
+        StartCoroutine(SpawnEnemyRoutine());
+    }
+
+    IEnumerator SpawnEnemyRoutine()
+    {
+        //spawn enemy de base.
+        yield return new WaitForSeconds(3.0f);
+        while (!_stopSpawning)
+        {
+            Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
+            GameObject newEnemy = Instantiate(_listeEnemyPrefabs[0], posToSpawn, Quaternion.identity);
+            newEnemy.transform.parent = _listeEnemyPrefabs[0].transform;
+            yield return new WaitForSeconds(5.0f);
+        }
+
+        //spawn mini boss.
+        yield return new WaitForSeconds(3.0f);
+        while (!_stopSpawning)
+        {
+            Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
+            GameObject newEnemy = Instantiate(_listeEnemyPrefabs[0], posToSpawn, Quaternion.identity);
+            newEnemy.transform.parent = _listeEnemyPrefabs[0].transform;
+            yield return new WaitForSeconds(5.0f);
+        }
+
+        //spawn enemy de base.
+        yield return new WaitForSeconds(3.0f);
+        while (!_stopSpawning)
+        {
+            Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
+            GameObject newEnemy = Instantiate(_listeEnemyPrefabs[0], posToSpawn, Quaternion.identity);
+            newEnemy.transform.parent = _listeEnemyPrefabs[0].transform;
+            yield return new WaitForSeconds(5.0f);
+        }
+
+    }
+
+    public void OnPlayerDeath()
+    {
+        _stopSpawning = true;
+    }
+}
